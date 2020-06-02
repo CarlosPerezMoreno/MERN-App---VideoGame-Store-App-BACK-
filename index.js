5e417aa42dbe9f09b9458e103a047c0cbc077bd8
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-02 18:52:39 +0200

    feat: New folders -> Routes, Controllers and Models
    Created these three new folders. First connection trying succesful.

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//Libraries
+
+const app = express();
+require("dotenv").config();
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Database Setup
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successful connection!");
+  });
+
+//Routes
+app.use("/api/category", require("./routes/category"));
+
+//Listen Port
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server in port: ${port}`);
+});
