010d5bffbd44772a2e9a82383a85a1c8012e473e
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-05 20:08:34 +0200

    feat: User registration connected properly
    Test realized with Postman. The sign in post runs okay

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,24 @@
-exports.test = (req, res) => {
-  console.log("testiiiing");
-  return res.json({ message: "woow!" });
-};
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 // Sign up
 
+exports.signup = (req, res) => {
+  console.log("req.body", req.body);
+  const user = new User(req.body);
+  user.save((error, save) => {
+    console.log("reached signup endpoint");
+    if (error) {
+      return res.status(400).json({
+        message: "Check fields, there is an error",
+      });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+    res.json({
+      user,
+    });
+  });
+};
 // Sign in
