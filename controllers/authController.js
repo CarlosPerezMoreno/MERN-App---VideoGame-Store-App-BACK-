4f1415366e85f37424bc620e6134288dc0dfe1a3
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-06 10:48:54 +0200

    feat: User auth. Fields verified. Signin set up.
    Backend has now the signin logic configured. Successfully tested by Postman.
    User auth needs permission. Token added for sign in session.

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,4 +21,39 @@ exports.signup = (req, res) => {
     });
   });
 };
+
 // Sign in
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error: "This email does not exist",
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Wrong password or email",
+      });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+    res.cookie("t", token, { expire: new Date() + 9999 });
+
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+};
+
+/* exports.isAdmin = (req, res, next) => {
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!user) {
+    return res.status(401).json({
+      error: "Access denied. You might need permission.",
+    });
+  }
+  next();
+};*/
